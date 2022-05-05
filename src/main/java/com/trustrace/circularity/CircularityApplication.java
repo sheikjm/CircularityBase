1e312f64d05a4bdd02b58ef5c141f3165e54bf72
sheik <sheik@10decoders.in>
2022-05-05 13:01:25 +0530

    fix

--- src/main/java/com/trustrace/circularity/CircularityApplication.java
+++ src/main/java/com/trustrace/circularity/CircularityApplication.java
@@ -1,10 +1,14 @@
 package com.trustrace.circularity;
 
+import com.trustrace.circularity.entity.User;
+import com.trustrace.circularity.repository.UserRepository;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,10 +16,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication()
 @OpenAPIDefinition(info = @Info(title = "Circularity API", version = "2.0", description = "Api Information"))
 @SecurityScheme(name = "circularity", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
-public class CircularityApplication {
+public class CircularityApplication implements CommandLineRunner {
+	@Autowired
+	UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CircularityApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		User user=new User();
+		user.setId(2L);
+		user.setRole("Admin");
+		user.setUsername("jsheik@gmail.com");
+		user.setPassword("123");
+
+		userRepository.save(user);
+
+	}
 }
